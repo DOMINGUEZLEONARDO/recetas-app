19c7f5c39803cfe6657418b80ec6641b3b5daae4
Leonardo <dominguezleonardo26@gmail.com>
2024-04-30 16:30:15 -0300

    Lastest modifications

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <div className='row'>
             <div className='com-titleWithfollow hlp-marginBottom-15'>
-                <h1 className="com-title-section-xl hlp-marginBottom-40">Acumulado</h1>
+                <h1 className="com-title-section-xl hlp-marginBottom-40">Acumulado Grilla</h1>
             </div>
         </div>
     )
